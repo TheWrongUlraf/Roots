7b7fff9623a48aa1818a194339f76efc3d41b66c
ulraf <nir.vaknin@mojang.com>
2023-02-04 17:52:35 +0100

    Grow a tree on every new root

---
+++ Assets/GrowTree.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowTree : MonoBehaviour
+{
+    float maxSize;
+    float growSpeed = 0.001f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        maxSize = UnityEngine.Random.Range(0.75f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localScale.x < maxSize)
+        {
+            transform.localScale += (Time.timeScale * new Vector3(growSpeed, growSpeed, growSpeed));
+        }
+    }
+}
