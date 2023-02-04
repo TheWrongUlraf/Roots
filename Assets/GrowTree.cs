d565a0110bc1cd6c4308c963d1f4109ad571db60
ulraf <nir.vaknin@mojang.com>
2023-02-04 22:50:20 +0100

    fix tree prefab

--- Assets/GrowTree.cs
+++ Assets/GrowTree.cs
@@ -11,6 +11,7 @@ public class GrowTree : MonoBehaviour
     {
         transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
         maxSize = UnityEngine.Random.Range(0.25f, 1f);
+        transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z + UnityEngine.Random.Range(-5f, 5f), transform.rotation.w);
     }
 
     // Update is called once per frame
