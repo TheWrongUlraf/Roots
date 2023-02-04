0967291d72e4cf85ad9fdc1219f75f47d56e5246
Kingblade <contact@kingbladegames.com>
2023-02-04 02:58:51 +0200

    created a simple looking level, with water patches and simple collision with tag checking

---
+++ Assets/TestScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Layer1Ground")
+        {
+            Debug.Log("Entered ground layer 1");
+        }
+        else if (other.gameObject.tag == "Layer2Ground")
+        {
+            Debug.Log("Entered ground layer 2");
+        }
+        else if (other.gameObject.tag == "Layer3Ground")
+        {
+            Debug.Log("Entered ground layer 3");
+        }
+        else if (other.gameObject.tag == "Water")
+        {
+            Debug.Log("Yummy yummy");
+        }
+    }
+}
