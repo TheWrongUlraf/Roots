e44b3d4707f56ba4867d3fb43742a9cb0413578a
ulraf <nir.vaknin@mojang.com>
2023-02-05 23:05:32 +0100

    trees tilt more right

--- Assets/GrowTree.cs
+++ Assets/GrowTree.cs
@@ -6,13 +6,13 @@ public class GrowTree : MonoBehaviour
 {
     float maxSize;
     float growSpeed = 0.001f;
-    public float anglesAllowed = 7.5f;
+    public float anglesAllowed = 5f;
     // Start is called before the first frame update
     void Start()
     {
         transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
         maxSize = UnityEngine.Random.Range(0.5f, 1f);
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, 0, Random.Range(-anglesAllowed, anglesAllowed * 1.5f)));
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, 0, Random.Range(-anglesAllowed * 3f, anglesAllowed)));
     }
 
     // Update is called once per frame
